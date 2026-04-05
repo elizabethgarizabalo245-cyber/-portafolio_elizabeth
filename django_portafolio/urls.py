3467752c4dbdd94c26e6176dccfd4e77ddb6cedf
TuNombre <tuemail@example.com>
2026-04-05 16:34:18 -0500

    test ruta

--- django_portafolio/urls.py
+++ django_portafolio/urls.py
@@ -16,14 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog import views
-from django.conf import settings
-from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def prueba(request):
+    return HttpResponse("SI FUNCIONA")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-]
-
-# para imágenes y archivos (CV)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', prueba),
+]  
