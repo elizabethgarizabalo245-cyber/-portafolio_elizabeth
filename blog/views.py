9acff4137577effc59dba5d4acd48d7260adc6f4
TuNombre <tuemail@example.com>
2026-04-05 16:40:36 -0500

    mini cambio

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Project
 
 def home(request):
-    try:
-        projects = Project.objects.all()
-    except:
-        projects = []
+    projects = Project.objects.all()
     return render(request, 'home.html', {'projects': projects})
--- django_portafolio/urls.py
+++ django_portafolio/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-def prueba(request):
-    return HttpResponse("SI FUNCIONA")
+from django.urls import path, include
+from blog import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', prueba),
-]  
+    path('', views.home, name='home'),
+]
+
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
