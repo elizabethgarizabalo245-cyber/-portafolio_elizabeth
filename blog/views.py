4a02131dcc6cf3b1fba0de10ead7e0972131a933
TuNombre <tuemail@example.com>
2026-04-06 18:12:54 -0500

    portafolio con perfil e imagenes corregidas

--- blog/views.py
+++ blog/views.py
@@ -2,36 +2,36 @@ from django.shortcuts import render
 
 def home(request):
     projects = [
-       
         {
             "title": "Diseño de Servicios Web",
             "description": "Proyecto en JavaScript enfocado en el diseño de servicios web.",
-            "image": "https://via.placeholder.com/300",
-            "url": "https://github.com/elizabethgarizabalo245-cyber/dise-o_serviciosweb.git"
+            "image": "https://via.placeholder.com/400x250",
+            "url": "https://github.com/elizabethgarizabalo245-cyber/dise-o_serviciosweb"
         },
         {
             "title": "Frontend Proyecto",
             "description": "Aplicación frontend desarrollada con JavaScript.",
-            "image": "https://via.placeholder.com/300",
-            "url": "https://github.com/elizabethgarizabalo245-cyber/frontend_proyecto.git"
+            "image": "https://via.placeholder.com/400x250",
+            "url": "https://github.com/elizabethgarizabalo245-cyber/frontend_proyecto"
         },
         {
             "title": "Proyecto Web Java",
             "description": "Aplicación web desarrollada en Java.",
-            "image": "https://via.placeholder.com/300",
-            "url": "https://github.com/elizabethgarizabalo245-cyber/ProyectoWebNuevo2.git"
+            "image": "https://via.placeholder.com/400x250",
+            "url": "https://github.com/elizabethgarizabalo245-cyber/ProyectoWebNuevo2"
         },
         {
             "title": "Proyecto Web",
             "description": "Proyecto web desarrollado en Java con funcionalidades básicas.",
-            "image": "https://via.placeholder.com/300",
-            "url": "https://github.com/elizabethgarizabalo245-cyber/proyecto-web.git"
+            "image": "https://via.placeholder.com/400x250",
+            "url": "https://github.com/elizabethgarizabalo245-cyber/proyecto-web"
         },
         {
             "title": "API Proyecto",
             "description": "API desarrollada con Node.js y Express para evidencia académica.",
-            "image": "https://via.placeholder.com/300",
-            "url": "https://github.com/elizabethgarizabalo245-cyber/api-proyecto..git"
+            "image": "https://via.placeholder.com/400x250",
+            "url": "https://github.com/elizabethgarizabalo245-cyber/api-proyecto"
         }
     ]
+
     return render(request, 'home.html', {"projects": projects})
