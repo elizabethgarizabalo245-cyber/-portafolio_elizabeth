4f156932998f99be0968e2b262f760d3a7d0a3a9
TuNombre <tuemail@example.com>
2026-04-05 21:37:24 -0500

    correccion

--- django_portafolio/urls.py
+++ django_portafolio/urls.py
@@ -16,12 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
-
-def test(request):
-    return HttpResponse("FUNCIONA PERFECTO")
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', test),
-]
+    path('', views.home, name='home'),
+] 
